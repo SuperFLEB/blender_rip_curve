0af7ba6216e66b17f0740ab20b54770237a77e2e
FLEB <voterudy@voterudy.org>
2022-11-20 13:52:48 -0500

    Support POLY and BEZIER curves

---
+++ src/lib/rip_curve.py
@@ -0,0 +1,90 @@
+import bpy
+from bpy.types import Object, Spline, SplinePoint, BezierSplinePoint
+from typing import Iterable, Sized
+
+def get_selected_spline(obj: Object):
+    """Return the first selected Bezier spline, or None if none is selected"""
+    for spline in obj.data.splines:
+        points = get_points_collection(spline)
+        attr = 'select_control_point' if spline.type == "BEZIER" else 'select'
+        for point in points:
+            if getattr(point, attr):
+                return spline
+    return None
+
+
+def copy_spline(obj: Object, spline: Spline) -> Spline:
+    """Copy the given spline. Clears the current selection and causes all points of the new spline to be seleted.
+       Returns the newly-created spline."""
+    bpy.ops.curve.select_all(action='DESELECT')
+    select_points(spline)
+    bpy.ops.curve.duplicate_move()
+    new_spline = get_selected_spline(obj)
+    return new_spline
+
+
+def get_selected_points_info() -> list[tuple[Object, Spline, SplinePoint | BezierSplinePoint]]:
+    """Get all selected points. Returns an (obj, spline, point) tuple"""
+
+    def is_sel(p: SplinePoint | BezierSplinePoint) -> bool:
+        for prop in sel_props:
+            if getattr(p, prop):
+                return True
+        return False
+
+    tuples = []
+    for ob in bpy.context.selected_objects:
+        if ob.type != "CURVE":
+            print("Unsupported object type", ob)
+            continue
+        for spl in ob.data.splines:
+            if spl.type not in ["BEZIER", "POLY"]:
+                print("Unsupported spline type", spl)
+                continue
+            all_points = get_points_collection(spl)
+            sel_props = ["select_control_point", "select_left_handle", "select_right_handle"] if spl.type == "BEZIER" else ["select"]
+            tuples.extend([(ob, spl, pt) for pt in all_points if is_sel(pt)])
+    return tuples
+
+
+def get_points_collection(spline: Spline) -> list[SplinePoint | BezierSplinePoint]:
+    """Get the points in the spline, regardless of whether they're Bezier or Poly"""
+    if spline.type == "BEZIER":
+        return list(spline.bezier_points)
+    return list(spline.points)
+
+def select_points(spline: Spline, indices: Iterable[int] = ()):
+    sel_props = {
+        "BEZIER": "select_control_point",
+        "POLY": "select"
+    }
+
+    if spline.type not in sel_props:
+        return
+
+    sel_prop = sel_props[spline.type]
+    coll = get_points_collection(spline)
+    indices = indices if indices else range(len(coll))
+
+    for idx in indices:
+        setattr(coll[idx], sel_prop, True)
+
+
+def split_on_point(obj: Object, spline: Spline, point: SplinePoint | BezierSplinePoint):
+    """Split the given obj's given spline on the given point. Changes the selection to the split point."""
+    points = spline.points if spline.points else spline.bezier_points
+    split_index = [idx for idx, pt in enumerate(points) if pt == point][0]
+    new_spline = copy_spline(obj, spline)
+    bpy.ops.curve.select_all(action='DESELECT')
+
+    point_count = len(spline.points if spline.points else spline.bezier_points)
+
+    # Select split_index+1 to len-1 of old spline for deletion, leaving 0--split_index
+    select_points(spline, range(split_index + 1, point_count))
+    # Select 0--(split_index-1) of new spline for deletion, leaving split_index--end
+    select_points(new_spline, range(split_index))
+
+    bpy.ops.curve.delete(type="VERT")
+
+    # Select the split_index on the old spline so the "same" point is selected once the operator completes.
+    select_points(spline, [split_index])
--- src/operator/rip.py
+++ src/operator/rip.py
@@ -2,18 +2,18 @@ import bpy
 from typing import Set
 from bpy.types import Operator
 from ..lib import pkginfo
-from ..lib import rip_bezier_curve
+from ..lib import rip_curve
 
 if "_LOADED" in locals():
     import importlib
 
-    for mod in (pkginfo, rip_bezier_curve):  # list all imports here
+    for mod in (pkginfo, rip_curve):  # list all imports here
         importlib.reload(mod)
 _LOADED = True
 
 package_name = pkginfo.package_name()
-get_active_points = rip_bezier_curve.get_active_points
-split_on_point = rip_bezier_curve.split_on_point
+get_selected_points_info = rip_curve.get_selected_points_info
+split_on_point = rip_curve.split_on_point
 
 
 class RipOperator(Operator):
@@ -24,7 +24,7 @@ class RipOperator(Operator):
 
     @classmethod
     def failcheck(cls):
-        active_points = get_active_points()
+        active_points = get_selected_points_info()
         active_points_ct = len(active_points)
 
         # This can occur if they selected the left or right control point. I _should_ handle this case, but I'm not yet.
@@ -36,11 +36,14 @@ class RipOperator(Operator):
         if active_points_ct > 1:
             return f"This operator can only rip a single point. {active_points_ct} are selected."
 
-        if active_points[0][2] == active_points[0][1].bezier_points[0]:
-            return f"Ripping the first point in a curve will accomplish nothing."
+        active_spline = active_points[0][1]
+        active_spline_points = active_spline.points if active_spline.points else active_spline.bezier_points
 
-        if active_points[0][2] == active_points[0][1].bezier_points[-1]:
-            return f"Ripping the last point in a curve will accomplish nothing."
+        if active_points[0][2] == active_spline_points[0]:
+            return f"Ripping the first point in a spline will accomplish nothing."
+
+        if active_points[0][2] == active_spline_points[-1]:
+            return f"Ripping the last point in a spline will accomplish nothing."
 
     @classmethod
     def poll(cls, context) -> bool:
@@ -56,12 +59,12 @@ class RipOperator(Operator):
         return True
 
     def execute(self, context) -> Set[str]:
-        active_points = get_active_points()
         fail = self.failcheck()
         if fail:
             self.report({'ERROR'}, fail)
             return {'FINISHED'}
 
+        active_points = get_selected_points_info()
         split_on_point(*active_points[0])
         return {'FINISHED'}
 
